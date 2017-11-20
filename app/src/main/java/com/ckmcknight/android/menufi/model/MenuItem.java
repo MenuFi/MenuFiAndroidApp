b7d431a089d60d52c44279c3d059c4aef39d7a56
Charlie McKnight <ckmcknight@gmail.com>
2017-11-20 13:45:23 -0500

    Started work on retrieving data from server

---
+++ app/src/main/java/com/ckmcknight/android/menufi/model/MenuItem.java
@@ -0,0 +1,8 @@
+package com.ckmcknight.android.menufi.model;
+
+/**
+ * Created by charlie on 11/20/17.
+ */
+
+public class MenuItem {
+}
---
+++ app/src/main/java/com/ckmcknight/android/menufi/model/datahandlers/MenuDataRetriever.java
@@ -0,0 +1,15 @@
+package com.ckmcknight.android.menufi.model.datahandlers;
+
+import com.ckmcknight.android.menufi.model.MenuItem;
+import com.ckmcknight.android.menufi.model.Restaurant;
+
+import java.util.List;
+
+/**
+ * Interface for explaining what data can be retrieved.
+ */
+public interface MenuDataRetriever {
+
+    List<Restaurant> getRestaurants();
+    List<MenuItem> getMenuItems(Restaurant restaurant);
+}
