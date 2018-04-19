4cb1f1afd00dab169acaa1a94cbb85e6622c4213
Connor Wyckoff <connorwyckoff@comcast.net>
2018-04-19 19:46:42 -0400

    Fixed Layout Issues
    # Conflicts:
    #	app/src/main/java/com/ckmcknight/android/menufi/controller/EditPreferenceFragment.java

--- app/src/main/java/com/ckmcknight/android/menufi/controller/EditAllergiesFragment.java
+++ app/src/main/java/com/ckmcknight/android/menufi/controller/EditAllergiesFragment.java
@@ -3,6 +3,7 @@ package com.ckmcknight.android.menufi.controller;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
